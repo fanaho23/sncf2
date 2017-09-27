f4f0aff37301d3c52e5b34b64983a112dc218151
fanaho23 <pierreche.thill@gmail.com>
2017-09-27 19:36:57 +0200

    Modification de ma view , pour afficher les formartion en fonction de l'activite selectionne en Ajax

--- application/views/v_Activite.php
+++ application/views/v_Activite.php
@@ -2,24 +2,24 @@
 
 <html>
     <head>
-        <title>TODO supply a title</title>
+        <title>sncf2</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
     </head>
     <body>
-        <div>TODO write content</div>
-        <h1><?php echo $activite ?></h1>
-    
-    <?php 
-            foreach ($lesActivites as $act) {
-                ?>
         
-        <a href ="index.php/CtrlSncf2/AfficherLesFormations/<?php echo $act->numero; ?>"><?php echo $act->libelle; ?><br></a>
-        <?php
+        <h1><?php echo $activite ?></h1>
+        <select id="lesActivites">
+            <?php 
+            foreach ($lesActivites as $act) {
+                
+        
+                   echo"<option value='".$act->numero."'>".$act->libelle."</option>";        
+        
             }
-    
-    
-    ?>
+            ?>
+        </select>
     
     
     
