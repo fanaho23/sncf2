3cd863e8c7968f248f82c4e02750c483a84bc6be
fanaho23 <pierreche.thill@gmail.com>
2017-09-27 19:38:21 +0200

    Ajout du lien dans ma view vers mon dossier JQuery

--- application/views/v_Activite.php
+++ application/views/v_Activite.php
@@ -5,7 +5,7 @@
         <title>sncf2</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        
+        <script type="text/javascript" src = "<?php echo base_url();?>JQuery/jquery-3.1.1.js"></script>
     </head>
     <body>
         
